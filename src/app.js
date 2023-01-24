6dda3ccce594bb663c8afc7a92b3a05cd8659463
Fernando-CR19 <fernandocr019@gmail.com>
2023-01-24 18:45:15 -0300

    24/01

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use("/auth", auth)
 app.use("/api/characters", characters)
